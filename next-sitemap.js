d706ccf65b79bbf52eb64e49d1ba2306a41d5e85
harshitsinghai77 <harshitsinghai77@gmail.com>
2022-05-09 18:21:48 +0530

    edit site url

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,12 +1,4 @@
 module.exports = {
-  siteUrl: 'https://startup-agency-beta.vercel.app',
+  siteUrl: "https://gre-word-meaning.vercel.app/",
   generateRobotsTxt: true,
-  // optional
-  // robotsTxtOptions: {
-  //   additionalSitemaps: [
-  //     'https://example.com/my-custom-sitemap-1.xml',
-  //     'https://example.com/my-custom-sitemap-2.xml',
-  //     'https://example.com/my-custom-sitemap-3.xml',
-  //   ],
-  // },
 };
